ef09fd6b9582788079b55b89adc71cc51b4192e0
DOSSANTOSDaniel <dossantosjdf@gmail.com>
2023-02-16 16:42:59 +0100

    Change all project

--- README.md
+++ README.md
@@ -1,67 +1,98 @@
-# Configuration personnelle macOS-Simple-KVM
-[macOS-Simple-KVM](https://github.com/foxlet/macOS-Simple-KVM) est un outil utilisant Qemu et KVM permettant d'émuler un système macOS, sur un PC, plusieurs version de macOS peuvent être installées par défaut, dont high-sierra, mojave et catalina.
-
-Pour information vous pouvez aussi installer l'application snap [Sosumi](https://snapcraft.io/sosumi) basé sur macOS-Simple-KVM.
-
+# Configuration personnelle de macOS-Simple-KVM
 ## Contexte
-On va installer macOS sur une machine virtuelle en utilisant un disque dur externe connecté par USB.
+[macOS-Simple-KVM](https://github.com/foxlet/macOS-Simple-KVM) est un outil utilisant Qemu et KVM permettant d'émuler un système macOS sur PC, plusieurs version de macOS peuvent être installées; high-sierra, mojave et catalina.
+
+Autres alternatives pour virtualiser macOS simplement :
+|Outil|Description|
+|---|---|
+|[Sosumi](https://snapcraft.io/sosumi)|Application snap basée sur le projet macOS-Simple-KVM.|
+|[Quickemu-project](https://github.com/quickemu-project/quickemu)|Outil utilisant Qemu et KVM permettant d'émuler plusieurs types de systèmes.|
+
+Le rôle de ce script est d'automatiser l'installation de macOS sur un disque dur physique connecté par port USB ou SATA au travers d'une configuration virtuelle à l'aide du projet macOS-Simple-KVM.
+
 Pour faire cette installation j’ai utilisé :
 * Ubuntu 22.04
 * QEMU emulator version 6.2.0
-* Un disque dur externe
+* Un disque dur physique connecté par port USB
 
-### Autres fonctionnalités
-1. Menu automatique permettant de faire le choix du disque dur à utiliser.
-2. Configuration du son.
+## Fonctionnalités
+1. Menu automatique permettant de récupérer le nom du disque dur à utiliser et la version de macOS à installer.
+2. Configuration de l'audio.
 3. Choix des paramètres automatisé.
-4. Configuration d'une connexion SSH.
+4. Connexion internet.
+5. Redirection du port 2222 vers le port 22 de la configuration virtuelle.
 
-## Vérification si la virtualisation est supportée sur le système
-```bash
-[[ $(grep -Ec '(vmx|svm)' /proc/cpuinfo) -gt 0 ]] && echo "Virtualisation supportée!" || echo "Virtualisation non supportée!"
+## Utilisation
+```
+./startmac.sh [-h|i]
+    
+Option 1: ./startmac.sh        Démarrage de la machine virtuelle.
+Option 2: ./startmac.sh -i     Télécharge macOS-Simple-KVM et l'image d'installation de macOS, puis démarre la machine virtuelle.
+Option 3: ./startmac.sh -h    Affiche l'aide.
 ```
 
-Si la virtualisation n'est pas supportée, voici quelques liens à suivre :
-* [Activation,désactivation de la virtualisation matérielle, HP](https://support.hp.com/fr-fr/document/ish_5637148-5698278-16)
-* [Activation,désactivation de la virtualisation matérielle, DELL](https://www.dell.com/support/kbdoc/fr-fr/000195978/activation-ou-desactivation-de-la-virtualisation-materielle-sur-les-systemes-dell)
-
-## Installation des dépendances
+1. Téléchargement du dépôt Ma_config_macOS-Simple-KVM
 ```bash
-sudo apt-get update && sudo apt-get install qemu-system qemu-utils python3 python3-pip bc git -y
+git clone https://github.com/DOSSANTOSDaniel/Ma_config_macOS-Simple-KVM.git
 ```
 
-## Téléchargement du dépôt macOS-Simple-KVM
+2. Démarrer une nouvelle installation
 ```bash
-git clone https://github.com/foxlet/macOS-Simple-KVM.git && cd macOS-Simple-KVM
+./startmac.sh -i 
 ```
 
-## Téléchargement de l'image d'installation
-Exemple avec la version catalina :
-```bash
-./jumpstart.sh --catalina
+3. Une fois l’image d’installation téléchargée et/ou le système installé sur le disque, alors on peut démarrer la machine virtuelle.
+ ```bash
+./startmac.sh
 ```
 
-## Lancer l'installation de macOS
-Copier le fichier basic.sh contenu dans le dossier Ma_config_macOS-Simple-KVM puis colle ce fichier dans le dossier macOS-Simple-KVM en écrasant l'ancien, puis lancer l'installation avec la commande.
-
-```bash
-./basic.sh
+## Connexion SSH (Fonctionne seulement entre la machine hôte et l'invité)
+ ```bash
+ssh -p 2222 <USER>@127.0.0.1
 ```
-Par la suite pour lancer la machine virtuelle macOS on utilise la même commande.
+
+## Installation de plusieurs versions de macOS
+Création des différents répertoires et copie du script.
+ ```bash
+for macver in catalina high-sierra mojave; do mkdir -p $macver && cp startmac.sh ${macver}; done
+```
+On peut maintenant lancer le téléchargement des différentes images et leur installation dans chaque répertoire.
+
+Voici un exemple :
+```
+Ma_config_macOS-Simple-KVM/
+├── catalina
+│   ├── installversion.log
+│   ├── macOS-Simple-KVM
+│   ├── README.md
+│   └── startmac.sh
+├── high-sierra
+│   ├── installversion.log
+│   ├── macOS-Simple-KVM
+│   ├── README.md
+│   └── startmac.sh
+└── mojave
+    ├── installversion.log
+    ├── macOS-Simple-KVM
+    ├── README.md
+    └── startmac.sh
+```
 
 ![demo](img.png)
 
-## Autre
-* Raccourci pour libérer le curseur : <kbd>Ctrl</kbd>+<kbd>Alt</kbd>+<kbd>g</kbd>
+## Raccourcis
+|Raccourci|Description|
+|---|---|
+|<kbd>Ctrl</kbd>+<kbd>Alt</kbd>+<kbd>g</kbd>|Libérer le curseur|
+|<kbd>Ctrl</kbd>+<kbd>Alt</kbd>+<kbd>f</kbd>|Plein écran|
+|<kbd>Ctrl</kbd>+<kbd>Alt</kbd>+<kbd>+</kbd>|Élargir l'écran|
+|<kbd>Ctrl</kbd>+<kbd>Alt</kbd>+<kbd>-</kbd>|Réduire l'écran|
+|<kbd>Ctrl</kbd>+<kbd>Alt</kbd>+<kbd>u</kbd>|Restaurer les dimensions de l'écran|
+
 * Faire le symbole pipe sur mac : <kbd>Alt</kbd>+<kbd>Shift</kbd>+<kbd>l</kbd>
 
 ## Aides
 * [Docummentation Qemu](https://qemu.readthedocs.io/en/latest/index.html)
 * [MacOS-Simple-KVM](https://github.com/foxlet/macOS-Simple-KVM)
-* [Debian-facile Qemu](https://debian-facile.org/doc:systeme:vm:qemu)
+* [Manpages debian Qemu](https://manpages.debian.org/testing/qemu-system-x86/qemu-system-x86_64.1.en.html)
 * [Archlinux Qemu](https://man.archlinux.org/man/qemu.1)
-
-## Améliorations
-- [ ] Mise en place du PCIe Passthrough.
-- [ ] Mise en place du Passthrough USB.
-- [ ] Optimiser la configuration de la VM...
---
+++ startmac.sh
@@ -0,0 +1,390 @@
+#!/bin/bash
+#-*- coding: UTF8 -*-
+
+#--------------------------------------------------#
+# Script_Name: startmac.sh	                               
+#                                                   
+# Date: mer. 15 févr. 2023 21:17                                             
+# Version: 1.0                                      
+# Bash_Version: 5.1.16(1)-release                           
+#--------------------------------------------------#
+#
+# Usage: ./startmac.sh [-h|i]
+#    
+# Option 1: ./startmac.sh        Démarrage de la machine virtuelle.
+# Option 2: ./startmac.sh -i     Télécharge macOS-Simple-KVM et l'image d'installation de macOS, puis démarre la machine virtuelle.
+# Option 3: ./startmac.sh -h     Affiche l'aide.
+#                                                                          
+#--------------------------------------------------#
+
+set -eu
+
+## Fonctions
+Usage() {
+  cat << USAGE
+
+    Usage: ${0} [-h|i]
+    
+    Option 1: ${0}        Démarrage de la machine virtuelle.
+    Option 2: ${0} -i     Télécharge macOS-Simple-KVM et l'image d'installation de macOS, puis démarre la machine virtuelle.
+    Option 3: ${0} -h     Affiche l'aide.
+
+USAGE
+  exit 0
+}
+
+# Install dependances
+InstallDep() {
+  echo "Téléchargement et installation des dépendances en cours ..."
+  if [[ "$OSTYPE" == 'linux-gnu' ]]
+  then
+    if (command -v apt-get 2> /dev/null)
+    then
+      for APPINSTALL in 'qemu-system-x86' 'qemu-utils' 'python3' 'python3-pip' 'bc' 'git' 'mesa-utils'
+      do
+        if ! (dpkg -L "${APPINSTALL}" 2> /dev/null)
+        then
+          sudo apt-get update -qq
+          if ! (sudo apt-get install ${APPINSTALL} -qqy)
+          then
+            echo "'ERREUR' Installation de ${APPINSTALL} Impossible!"
+            exit 1
+          fi
+        fi
+      done
+    elif (command -v dnf 2> /dev/null)
+    then
+      for APPINSTALL in 'qemu' 'qemu-img' 'python3' 'python3-pip' 'bc' 'git' 'glx-utils'
+      do  
+        if ! (rpm -ql ${APPINSTALL} 2> /dev/null)
+        then  
+          sudo dnf check-update --quiet
+          if ! (sudo dnf install --quiet --assumeyes ${APPINSTALL})
+          then
+            echo "'ERREUR' Installation de ${APPINSTALL} Impossible!"
+            exit 1
+          fi
+        fi
+      done
+    elif (command -v pacman 2> /dev/null)
+    then
+      for APPINSTALL in 'qemu' 'python' 'python-pip' 'python-wheel' 'bc' 'git' 'glxinfo' #qemu-headless
+      do  
+        if ! (pacman -Ql ${APPINSTALL} 2> /dev/null)
+        then
+          sudo pacman -Sy --quiet 
+          if ! (sudo pacman -S ${APPINSTALL} --quiet --noconfirm --noprogressbar)
+          then
+            echo " 'ERREUR' Installation de ${APPINSTALL} Impossible!"
+            exit 1
+          fi
+        fi
+      done
+    else
+      echo -e "\n Gestionnaire de paquets non prit en charge."
+      echo -e "Merci d'installer ${APPINSTALL} manuellement. \n"
+      exit 1
+    fi
+  else
+    echo -e "\n Système non prit en charge. \n"
+    exit 1
+  fi
+}
+
+# Download macOS-Simple-KVM
+DownloadMacSimple() {
+  if ! [[ -d ${MSKDIR} ]]
+  then
+    if ! (git clone https://github.com/foxlet/macOS-Simple-KVM.git)
+    then
+      echo " 'ERREUR' Téléchargement de https://github.com/foxlet/macOS-Simple-KVM.git Impossible!"
+      exit 1
+    fi
+  fi
+}
+
+# Choice of macOS version : high-sierra, mojave or catalina
+DownloadMacVersion() {
+  PS3="Votre choix : "
+  clear
+  echo " ----- Menu macOS versions ----- "
+  echo
+  select ITEM in 'high-sierra' 'mojave' 'catalina' 'Quitter'
+  do
+    if [[ $ITEM == 'Quitter' ]]
+    then
+      echo "Fin du programme!"
+      exit 0
+    fi
+    break
+  done
+
+  MACVER="$ITEM"
+
+  cd $MSKDIR
+  # Download installation image
+  ./jumpstart.sh --${MACVER}
+  date +%c >> ${MSKPDIR}/installversion.log
+  echo "macOS $MACVER" >> ${MSKPDIR}/installversion.log
+  echo "###" >> ${MSKPDIR}/installversion.log
+  
+  BANVERSIONMAC="Version :----------------: macOS $MACVER"
+  
+  read -p "Voulez vous installer macOS $MACVER sur le disque maintenant [o/n] ? : " CHECKIFINSTALL
+  if ! [[ $CHECKIFINSTALL =~ ^(o|O|)$ ]]
+  then
+    echo "Fin du programme"
+    exit 1
+  fi
+}
+
+### Main ###
+
+## Global variables
+# Installation directory
+MSKPDIR="$(pwd)"
+MSKDIR="${MSKPDIR}/macOS-Simple-KVM"
+
+# User
+IDHOSTUSER="$(id -u)"
+HOSTUSER="$(id -u $IDHOSTUSER -n)"
+
+# Banner
+BANVERSIONMAC='...'
+BANDESCRIPTIONDEVICE='...'
+BANUSBHOSTBUS='...'
+BANUSBHOSTPORT='...'
+
+# Audio, ex: pa,alsa,jack,oss,sdl,none
+QEMUAUDIO='pa'
+QEMUAUDIOSERVER="/run/user/${IDHOSTUSER}/pulse/native" 
+
+# Port SSH : ssh -p 2222 user@127.0.0.1
+HOSTSSHPORT='2222'
+USB_OPTION=" "
+
+# OpenGL
+GLSUPPORT='on'
+
+# BIOS
+OSK='ourhardworkbythesewordsguardedpleasedontsteal(c)AppleComputerInc'
+VMDIR=$MSKDIR
+OVMF=${VMDIR}/firmware
+
+# RAM
+PCRAM="$(grep 'MemTotal' /proc/meminfo | awk '{print $2}')"
+VMRAM="$(echo "scale = 10;ram=${PCRAM};ram/=1024;ram/=1024;ram/=2;ram" | bc | awk '{printf("%d\n",$1 + 0.5)}')G"
+
+# CPU
+MODELCPU="$(lscpu | grep -E 'Model name|Nom de modèle' | tr -s " " | cut -d " " -f4-)"
+CPUTHREADS="$(lscpu | grep -E 'Thread\(s\) per core|Thread\(s\) par cœur' | awk '{print $4}')"
+CPUCORES="$(lscpu | grep -E 'Core\(s\) per socket|Cœur\(s\) par socket' | awk '{print $4}')"
+CPUSOCKETS="$(lscpu | grep 'Socket(s)' | awk '{print $2}')"
+TOTALTHREADS="$((CPUTHREADS * CPUCORES * CPUSOCKETS))"
+CPUOPTIONS='+kvm_pv_unhalt,+popcnt,+kvm_pv_eoi,+hypervisor,vmware-cpuid-freq=on,+invtsc,+pcid,+ssse3,+sse4.2,+avx,+avx2,+aes,+fma,+fma4,+bmi1,+bmi2,+xsave,+xsaveopt,+sse3,+xsavec,+xgetbv1,+smep,+movbe,check'
+
+## Tests
+# Check user
+if [[ $(id -u) -eq 0 ]]
+then
+  clear
+  echo -e "\n Ne pas lancer le script en tant que root ! \n"
+  exit 0
+fi
+
+# Checking if virtualization is supported
+if [[ $(grep -Ec '(vmx|svm)' /proc/cpuinfo) -gt 0 ]]
+then
+  echo "Virtualisation supportée!"
+else
+  echo "La virtualisation n'est pas supportée sur ce système !"
+  echo "Activer la technologie de virtualisation (VTx) dans le BIOS"
+  exit 1
+fi
+
+readonly PARAM="iuh"
+while getopts "${PARAM}" PARG
+do
+  case "${PARG}" in
+    h)
+      clear
+      Usage
+      ;;
+    i)
+      InstallDep
+      DownloadMacSimple
+      if [[ -f ${MSKDIR}/BaseSystem.img ]]
+      then
+        clear
+        cat ${MSKPDIR}/installversion.log
+        echo -e "\n Une image d'installation existe déjà !"
+        echo " Pour télécharger une nouvelle image, il faut placer le script ${0} dans un répertoire différent, puis lancer le téléchargement."
+        echo -e " Si non vous pouvez écraser l'image actuelle pour télécharger la nouvelle. \n"
+        read -p "Voulez vous écraser l'image actuelle ? [o/n] : " REPERASE
+        if [[ $REPERASE =~ ^(o|O|)$ ]]
+        then
+          DownloadMacVersion
+        else
+          echo "Fin du programme"
+          exit 1
+        fi
+      else
+        DownloadMacVersion
+      fi
+      ;;
+    *)
+      echo "Option non valide !"
+      ;;
+  esac
+done
+
+if [[ -d $MSKDIR ]]
+then
+  cd $MSKDIR
+else
+  clear
+  echo -e "\n Le dossier $MSKDIR n'existe pas !"
+  echo -e " Il faut faire une installation : ${0} -i \n"
+  Usage
+fi
+
+# Choosing a physical disk
+read -p "Brancher votre disque puis appuyez sur Entrée ! "
+
+PS3="Votre choix : "
+mapfile -t USBDEVICES < <(lsblk -l --noheadings --perms --nodeps --exclude 7 --output NAME,SIZE,TRAN,SERIAL,MODEL)
+clear
+echo " ----- Menu disques ----- "
+echo
+echo "   NAME   SIZE TRAN   SERIAL       MODEL"
+select ITEM in "${USBDEVICES[@]}" 'Quitter'
+do
+  if [[ $ITEM == 'Quitter' ]]
+  then
+    echo "Fin du programme!"
+    exit 0
+  fi
+  break
+done
+
+# Check if the disk is in use by the system
+INFODISK="$ITEM"
+NAMEDISK="$(echo $ITEM | awk '{print $1}')"
+
+mapfile -t LISTMOUNTDISK < <(findmnt --fstab --evaluate --noheadings | awk '{print $2}' | tr -d "[:digit:]" | uniq)
+
+for MOUNTDISK in "${LISTMOUNTDISK[@]}"
+do
+  if [[ "$MOUNTDISK" = "/dev/${NAMEDISK}" ]]
+  then
+    echo "Attention disque en cours d'utilisation par le système !"
+    exit 1
+  fi
+done
+
+# Get name disk by ID
+mapfile -t IDDISK < <(ls -l /dev/disk/by-id/*)
+
+for testname in "${IDDISK[@]}"
+do
+  if [[ "$testname" = *${NAMEDISK} ]] && [[ "$testname" = *wwn* ]]
+  then
+    PHYSICALDISK="$(echo $testname | awk '{print $9}')"
+    break
+  else
+    if [[ "$testname" = *${NAMEDISK} ]]
+    then
+      PHYSICALDISK="$(echo $testname | awk '{print $9}')"
+    fi
+  fi
+done
+
+if [[ -z $PHYSICALDISK ]]
+then
+  PHYSICALDISK="/dev/$NAMEDISK"
+fi
+
+# Set disk permissions
+set +e
+sudo umount /dev/${NAMEDISK}*
+set -e 
+sudo chown ${HOSTUSER}:${HOSTUSER} $PHYSICALDISK
+
+# If OpenGL ES profile shading language is too old, GLSL ES 3.00 is not supported
+GLVERSION=$(glxinfo -B | grep 'OpenGL ES profile shading language version string' | awk '{print $12}' | cut -d'.' -f1)
+if [[ $GLVERSION -lt 3 ]]
+then 
+  GLSUPPORT='off'
+fi
+
+clear
+
+cat << EOF
+           ___ $(basename ${0}) ___
+       
+  ----------------- Résumé -----------------
+  ------------------------------------------
+  
+  Host User :--------------: $HOSTUSER
+  
+  Host Infos Disk :--------: /dev/${INFODISK}
+  
+  $BANVERSIONMAC
+  
+  Host OpenGL ES :---------: $GLSUPPORT
+  
+  RAM vm :-----------------: $VMRAM
+  
+  Host CPU Model :---------: $MODELCPU
+  
+  Vm CPU Threads :---------: $CPUTHREADS
+  Vm CPU_Cores :-----------: $CPUCORES
+  Vm CPU_Sockets :---------: $CPUSOCKETS
+  Vm CPU_Total_Threads :---: $TOTALTHREADS
+  
+  Connexion SSH :----------: ssh -p 2222 <USER>@127.0.0.1
+  
+  Host AUDIO_Device :------: $QEMUAUDIO
+  AUDIO_Server :-----------: $QEMUAUDIOSERVER
+
+  
+EOF
+
+read -p "Appuyez sur Entrée pour continuer ! "
+
+qemu-system-x86_64 \
+    -name macos,process='macos' \
+    -pidfile ${MSKPDIR}/macos.pid \
+    -boot menu=on \
+    -enable-kvm \
+    -m ${VMRAM} \
+    -machine q35,smm=off,vmport=off,accel='kvm' \
+    -smp ${TOTALTHREADS:='4'},threads="${CPUTHREADS:='2'}",cores="${CPUCORES:='2'}",sockets="${CPUSOCKETS:='1'}" \
+    -global kvm-pit.lost_tick_policy='discard' \
+    -global ICH9-LPC.disable_s3='1' \
+    -cpu Penryn,kvm='on',vendor='GenuineIntel',${CPUOPTIONS} \
+    -device isa-applesmc,osk="$OSK" \
+    -smbios type='2' \
+    -rtc base='localtime',clock='host',driftfix='slew' \
+    -object rng-random,id='rng0',filename='/dev/urandom' \
+    -device virtio-rng-pci,rng='rng0' \
+    -drive if='pflash',format='raw',readonly='on',file="${OVMF}/OVMF_CODE.fd" \
+    -drive if='pflash',format='raw',file="${OVMF}/OVMF_VARS-1024x768.fd" \
+    -display gtk,window-close='on',gl="$GLSUPPORT" \
+    -device qxl-vga,vgamem_mb='128' \
+    -k fr \
+    -L /usr/share/seabios/ \
+    -L /usr/lib/ipxe/qemu/ \
+    -audiodev ${QEMUAUDIO},id="$QEMUAUDIO",server="$QEMUAUDIOSERVER" \
+    -device ich9-intel-hda \
+    -device hda-duplex,audiodev="$QEMUAUDIO" \
+    -usb -device usb-kbd -device usb-tablet \
+    -netdev user,id='net0',hostfwd="tcp:127.0.0.1:${HOSTSSHPORT}-:22" \
+    -device vmxnet3,netdev='net0',id='net0' \
+    -device ich9-ahci,id='sata' \
+    -drive id='ESP',if='none',format='qcow2',file='ESP.qcow2' \
+    -device ide-hd,bus='sata.2',drive='ESP' \
+    -drive id='InstallMedia',format='raw',if='none',file='BaseSystem.img' \
+    -device ide-hd,bus='sata.3',drive='InstallMedia' \
+    -drive id='SystemDisk',if='none',file="$PHYSICALDISK",format='raw',cache=writeback,media='disk' \
+    -device ide-hd,bus='sata.4',drive='SystemDisk' &
